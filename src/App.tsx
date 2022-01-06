34fb03a5e7ad1b9c8a5f6b4aee4518a2a0f53c44
otinomz <82697198+otinomz@users.noreply.github.com>
2022-01-06 11:17:52 +0100

    importing data from the data file

--- src/App.tsx
+++ src/App.tsx
@@ -10,12 +10,12 @@ import {
   Tooltip,
   Crosshair
 } from "@syncfusion/ej2-react-charts";
+import { chartData } from "./data";
 
 export default function App() {
   return (
     <div className="App">
       <h1>Financial Charts with Sync function</h1>
-
       <ChartComponent>
         <Inject services={[HiloSeries]}></Inject>
         <SeriesCollectionDirective>
